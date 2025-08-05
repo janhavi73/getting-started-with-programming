97f7af249916796f9a1b3d0d901058888aafc640
janhavi73 <janhavisinghania0607@gmail.com>
2025-08-05 14:08:06 +0100

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,20 @@
+name="Penguin"
+age=10
+is_student=True
+weight=38.5
+
+print("Name:",name)
+print("type of name",type(name))
+print("Age:",age)
+print("type of age",type(age))
+print("Is student:",is_student)
+print("type of is student",type(is_student))
+print("Weight",weight)
+print("type of weight:",type(weight))
+
+print("type casting age")
+age=float(age)
+print(age,type(age))
+
+weight=str(weight)
+print(weight,type(weight))
