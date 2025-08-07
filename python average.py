a63efa99fad70c17aac5f14689e302b96ad418bb
janhavi73 <janhavisinghania0607@gmail.com>
2025-08-07 11:17:05 +0100

    Add files via upload

---
+++ python average.py
@@ -0,0 +1,11 @@
+tree1=73
+tree2=27
+tree3=56
+tree4=44
+tree5=100
+
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum of these numbers are:",sum)
+
+average=sum/5
+print("the average of these numbers are:", average)
